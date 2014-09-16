b1c31b71522993b0a872fc014bd99586021b4e3f
1upsoda <1upsoda@gmail.com>
2014-09-16 08:33:34 -0600

    Added stuff
    another if else if age 42

--- src/questions/controller/questions.java
+++ src/questions/controller/questions.java
@@ -31,24 +31,33 @@ public class questions
 		answerGUI = JOptionPane.showInputDialog("Hi, " + answerGUI + " How are you today?");
 		answerGUI = JOptionPane.showInputDialog("''" + answerGUI + "''? Well that's good, what's your favorite color?");
 		answerGUI = JOptionPane.showInputDialog("I don't like the color " + answerGUI + " because I am colorblind... Now tell me your age:");
-		JOptionPane.showMessageDialog(null, "The bot has fled");
-		JOptionPane.showMessageDialog(null, "He ain't comming back...");
-		JOptionPane.showMessageDialog(null, "Like, forever...");
-		JOptionPane.showMessageDialog(null, "What did you say to him?");
-		answerGUI = JOptionPane.showInputDialog("That you are " + answerGUI + " years old? He for some reason hates the number " +answerGUI+ "... Anyway, what is YOUR favorite number?");
 		if(answerGUI.equals("42"))
 		{
-			JOptionPane.showMessageDialog(null, "The replacement bot has left...");
+			JOptionPane.showMessageDialog(null, "What a peculiar age...");
+			JOptionPane.showMessageDialog(null, "Yeah, I am just gonna leave...");
 		}
 		else
 		{
-			answerGUI = JOptionPane.showInputDialog(null, "Oh the number " +answerGUI+ " is pretty cool, what is/was your favorite subject in school?");
-			answerGUI = JOptionPane.showInputDialog("I hated all subjects in school, especially " +answerGUI+ ". What subject did/do you hate?");
-			answerGUI = JOptionPane.showInputDialog("I never took " +answerGUI+ " so I wouldn't know how it's like... What is your favorite possesion?");
-			answerGUI = JOptionPane.showInputDialog("That sounds like a very cool object. Where can I get a(n) ''" +answerGUI+ "''?");
-			JOptionPane.showMessageDialog(null, "Well " +answerGUI+ " Seems too far away, I am just going to take yours...");
-			JOptionPane.showMessageDialog(null, "The replacement bot has left...");
+			JOptionPane.showMessageDialog(null, "The bot has fled");
+			JOptionPane.showMessageDialog(null, "He ain't comming back...");
+			JOptionPane.showMessageDialog(null, "Like, forever...");
+			JOptionPane.showMessageDialog(null, "What did you say to him?");
+			answerGUI = JOptionPane.showInputDialog("That you are " + answerGUI + " years old? He for some reason hates the number " +answerGUI+ "... Anyway, what is YOUR favorite number?");
+			if(answerGUI.equals("42"))
+			{
+				JOptionPane.showMessageDialog(null, "The replacement bot has left...");
+			}
+			else
+			{
+				answerGUI = JOptionPane.showInputDialog(null, "Oh the number " +answerGUI+ " is pretty cool, what is/was your favorite subject in school?");
+				answerGUI = JOptionPane.showInputDialog("I hated all subjects in school, especially " +answerGUI+ ". What subject did/do you hate?");
+				answerGUI = JOptionPane.showInputDialog("I never took " +answerGUI+ " so I wouldn't know how it's like... What is your favorite possesion?");
+				answerGUI = JOptionPane.showInputDialog("That sounds like a very cool object. Where can I get a(n) ''" +answerGUI+ "''?");
+				JOptionPane.showMessageDialog(null, "Well " +answerGUI+ " Seems too far away, I am just going to take yours...");
+				JOptionPane.showMessageDialog(null, "The replacement bot has left...");
+			}
 		}
+		
 		
 	}
 	
